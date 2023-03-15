fa962e9021a6c47a864aefb630c860dcd74d5b7e
Saulo Machado <saulo.machado@g.globo>
2023-03-15 17:07:38 -0300

    Filtrando grade de acordo com os horários que cabem no budget do cliente

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -22,33 +22,35 @@ export class UserComponent implements OnInit {
   }
 
   callAPI() {
+    let budget = 60000;
+    let seconds = 10; // Quantidade de segundos da duração do anúncio a ser exibido
+    let targetAudience; // Público alvo do anúncio para ordenar os 
+    let keyWords;
 
-      let budget;
-      let seconds; // Quantidade de segundos da duração do anúncio a ser exibido
-      let targetAudience; // Público alvo do anúncio para ordenar os 
-      let keyWords;
+    this.apiService.getCanais().subscribe((res: any) => {
+      this.canais = res;
+      this.apiService.getCusto().subscribe((res: any) => {
+        this.custos = res;
+        this.apiService.getGrade().subscribe((res: any) => {
+          this.grade = res;
+          this.apiService.getAudiencia().subscribe((res: any) => {
+            this.audiencia = res;
 
-      let timesByBudget = this.custos.filter(e => e.value <= budget);
-      let times = timesByBudget.map(e => e.time.split(":")[0]);
-      let gradesByTime = this.grade.filter(e => times.includes(e.startTime.split(":")));
-      console.log(gradesByTime);
+            console.log("Grade: ", this.grade[0])
+            console.log("Custos: ", this.custos[0])
 
-      this.apiService.getCanais().subscribe((res: any) => {
-        this.canais = res;
-        this.apiService.getCusto().subscribe((res: any) => {
-          this.custos = res;
-          this.apiService.getGrade().subscribe((res: any) => {
-            this.grade = res;
-            this.apiService.getAudiencia().subscribe((res: any) => {
-              this.audiencia = res;
-            }, err => {
-            });
+            let timesByBudget = this.custos.filter(e => e.value * seconds <= budget);
+            let times = timesByBudget.map(e => e.time.split(":")[0]);
+            console.log("Times: ", times);
+            let gradesByTime = this.grade.filter(e => times.includes(e.startTime.split(":")[0]));
+            console.log(gradesByTime);
           }, err => {
           });
         }, err => {
         });
       }, err => {
       });
-
-    }
+    }, err => {
+    });
+  }
 }
