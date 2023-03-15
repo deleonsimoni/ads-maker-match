5435455b854e3b50938143961638f202211ce055
Deleon Simoni <deleon.simoni>
2023-03-15 16:17:06 -0300

    wip: processing ads maker

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,11 +1,73 @@
 import { Component, OnInit } from "@angular/core";
+import { ApisService } from "src/app/services/apis.service";
 
 @Component({
   selector: "app-user",
   templateUrl: "user.component.html"
 })
 export class UserComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  custos;
+  canais;
+  grade;
+  audiencia;
+
+  constructor(
+    private apiService: ApisService,
+  ) { }
+
+  ngOnInit() {
+
+    this.callAPI();
+
+  }
+
+  time2Seconds(time) {
+    let resposta = time.split(":");
+    return resposta[0] * 3600 + resposta[1] * 60 + resposta[2];
+  }
+
+  callAPI() {
+
+    let budget;
+    let targetAudience;
+    let keyWords;
+
+    let timesByBudget = this.custos.filter(e => e.value <= budget);
+
+    timesByBudget.map(times => {
+      times.inicio = this.time2Seconds(times.time);
+      let tempoFim = times.time.split(":")[0] + ":59:59";
+      times.fim = this.time2Seconds(tempoFim);
+    })
+
+
+    let gradesByTime = this.grade.filter(e => this.time2Seconds(e.startTime) >= times.timeInicio && this.time2Seconds(e.startTime) <= times.timeFim);
+
+
+
+    this.apiService.getCanais().subscribe((res: any) => {
+      this.canais = res;
+    }, err => {
+
+    });
+
+    this.apiService.getCusto().subscribe((res: any) => {
+      this.custos = res;
+    }, err => {
+
+    });
+
+    this.apiService.getGrade().subscribe((res: any) => {
+      this.grade = res;
+    }, err => {
+
+    });
+
+    this.apiService.getAudiencia().subscribe((res: any) => {
+      this.audiencia = res;
+    }, err => {
+
+    });
+  }
 }
